d04549dcbbfcd7d2c454a7693dd48b4c76b71fc0
sanketh2222 <sankeths94@gmail.com>
2020-05-31 12:44:54 +0530

    first commit

--- s3proj/s3proj_stack.py
+++ s3proj/s3proj_stack.py
@@ -1,4 +1,7 @@
-from aws_cdk import core
+from aws_cdk import ( 
+    aws_s3 as _s3,
+    core
+)
 
 
 class S3ProjStack(core.Stack):
@@ -7,3 +10,12 @@ class S3ProjStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+        _s3.Bucket(
+            self,
+            "MyBucket",
+            bucket_name="myfirstbucket2808",
+            versioned=True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+            
+            
+        )
